69ac0d9309d5a282972a07f5249e0ce286ee6d61
Karol Dydo <karoldydo@gmail.com>
2025-10-11 22:33:30 +0200

    feat: integrate Vitest for testing with coverage support and update project configuration

---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig, mergeConfig } from 'vitest/config';
+
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      coverage: {
+        provider: 'v8',
+        reporter: ['text-summary', 'html', 'lcov'],
+      },
+      environment: 'jsdom',
+      globals: true,
+      setupFiles: ['./src/test/setup.ts'],
+      watch: false,
+    },
+  })
+);
